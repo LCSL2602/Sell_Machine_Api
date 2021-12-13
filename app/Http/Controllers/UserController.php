2d08d2e0c32f6f876ec0562ad75d98085ee9dfce
Luis Sulbaran <luis.sulbaran@emqu.net>
2021-12-13 08:22:23 -0400

    first controller functional

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,29 +1,53 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index( Request $request){
-        var_dump('get all users');
+        $Users = User::all();
+        return $Users;
     }
 
     public function store( Request $request){
-        var_dump('Create user');
-        var_dump($request->all());
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = $request->password;
+            $user->lastname = $request->lastname;
+            $user->rut = $request->rut;
+            $user->rol = 'Executive';
+            
+            $user->save();
+            $createAt['create'] = True;
+            return $createAt ;
     }
 
     public function getUser( Request $request , $id){
-        var_dump('get user '.$id );
+       $user = User::where('id', $id)->get();
+       return $user;
     }
 
     public function update( Request $request, $id){
-        var_dump('Update user '.$id );
+        $user = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = $request->password;
+            $user->lastname = $request->lastname;
+            $user->rut = $request->rut;
+           
+            $user->save();
+            $updateAt['update'] = True;
+            return $updateAt ;
+    
     }
 
     public function remove( Request $request, $id){
-        var_dump('Delete user '.$id );
+        $user = User::find($id);
+        $user->delete();
+        $daleteeAt['delete'] = True;
+        return  $daleteeAt;
     }
 }
